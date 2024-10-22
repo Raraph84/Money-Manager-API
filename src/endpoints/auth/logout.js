c542f434cdd53a37068b951e2276ca39d014ecf1
Raraph84 <contact@raraph.fr>
2024-10-22 22:47:01 +0200

    Add logout endpoint

---
+++ src/endpoints/auth/logout.js
@@ -0,0 +1,22 @@
+/**
+ * @param {import("raraph84-lib/src/Request")} request 
+ * @param {import("mysql2/promise").Pool} database 
+ */
+module.exports.run = async (request, database) => {
+
+    try {
+        await database.query("DELETE FROM tokens WHERE token=?", [request.headers.authorization]);
+    } catch (error) {
+        request.end(500, "Internal server error");
+        console.log(`SQL Error - ${__filename} - ${error}`);
+        return;
+    }
+
+    request.end(204);
+}
+
+module.exports.infos = {
+    path: "/auth/logout",
+    method: "POST",
+    requiresAuth: true
+}
